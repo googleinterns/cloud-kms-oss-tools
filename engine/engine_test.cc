8324c8fb9583dfa2ea302279c368259d1c148c0e
Zachary Espiritu <zesp@google.com>
2020-06-03 21:13:03 +0000

    Add note to engine_test about dynamic linking

--- engine/engine_test.cc
+++ engine/engine_test.cc
@@ -24,6 +24,16 @@ TEST(EngineTest, EngineInitializes) {
     OpenSSL_add_all_algorithms();
     ERR_load_crypto_strings();
 
+    // TODO: This is going to require engine.so to be within the ENGINESDIR of
+    // openssl (see `openssl version -e`), so not sure how to do this with integration
+    // testing in a modular way (for now I can just create a symlink to engine.so in
+    // the ENGINESDIR).
+    //
+    // Alternatively, something that would be nice to have is that ENGINE_load_dynamic
+    // (or some variant of it) can just take a path directly to an *.so object or
+    // an OpenSSL configuration file that specifies both the dynamic library path as
+    // well as whatever HSM configuration is necessary (which presumably, will be
+    // necessary for some integration tests).
     ENGINE_load_dynamic();
     ENGINE *engine = ENGINE_by_id("hsm");
     ASSERT_TRUE(engine);
