38c6b46f49413c25977755f7389fd69e2a390281
Zachary Espiritu <zesp@google.com>
2020-06-15 21:49:33 +0000

    Add back in pure C wrapper

---
+++ engine/wrapper.cc
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "wrapper.h"
+
+#include "openssl/engine.h"
+
+#include "engine/engine.h"
+
+extern "C" {
+// Binds the Cloud KMS engine implementations to the input ENGINE struct from
+// OpenSSL. Returns 1 on success and 0 if an error occured.
+//
+// This is the main entry point from OpenSSL into the Cloud KMS Engine. OpenSSL
+// calls this function when the engine shared object is loaded and passes it a
+// fresh, non-null ENGINE struct. It is our code's responsibility to initialize
+// the input struct using OpenSSL's ENGINE_set_* API functions.
+int bind_function(ENGINE *e, const char *id) {
+  return engine::Engine::BindOpenSSLEngine(e);
+}
+
+
+}
---
+++ engine/wrapper.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ENGINE_WRAPPER_H_
+#define ENGINE_WRAPPER_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif  // __cplusplus
+
+#include "openssl/engine.h"
+
+int bind_function(ENGINE *e, const char *id);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // ENGINE_WRAPPER_H
