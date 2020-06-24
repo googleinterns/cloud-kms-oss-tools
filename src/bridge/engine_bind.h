3bb7a5c325b54129b9cba3e27611ef5f7f3a5c9c
Zachary Espiritu <zesp@google.com>
2020-06-24 16:01:56 +0000

    Implement "pure C wrapper" entry point into minimal C++ engine (#30)
    * Add minimal useless engine with C++ structure
    
    * Add tests for BindEngine
    
    * Add integration test for dynamically loading engine
    
    * Update WORKSPACE for protobuf and grpc dependencies
    
    * Add TODOs to EngineInit and EngineFinish
    
    * Remove OpenSSL CLI integration test for now
    
    * Use constexpr on engine names
    
    * Update BUILD files to adhere to Bazel style guide
    
    * Clarify comment in top-level BUILD file
    
    * Update bind structure
    
    * Set canonical version of com_google_googleapis in WORKSPACE
    
    * Add kmsengine top-level namespace
    
    * Move libengine namespace to engine namespace
    
    * Simplify engine_bind_test
    
    * Rename namespace engine to namespace openssl_interface
    
    * Add more detailed comment to com_googleapis_google_cloud_cpp dependency
    
    * Rename openssl_interface namespace to bridge
    
    * Rename header macros

---
+++ src/bridge/engine_bind.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KMSENGINE_BRIDGE_ENGINE_BIND_H_
+#define KMSENGINE_BRIDGE_ENGINE_BIND_H_
+
+#include <openssl/engine.h>
+
+// Binds the Cloud KMS engine implementations to the input ENGINE struct from
+// OpenSSL. Returns 1 on success and 0 if an error occured.
+//
+// This is the main entry point from OpenSSL into the Cloud KMS Engine. OpenSSL
+// calls this function when the engine shared object is loaded and passes it a
+// fresh, non-null ENGINE struct. Our code is responsible for initializing
+// the input struct using OpenSSL's ENGINE_set_* API functions.
+//
+// This function is exposed as a pure C name (via `extern "C"`) so that we can
+// pass the OpenSSL IMPLEMENT_DYNAMIC_BIND_FN macro a pure C name as opposed to
+// a C++ name. Passing a C++ name to the macro directly may cause C++'s
+// name-mangling to break the macro; for example, if the macro implementation
+// defines new functions based on the name of the input function.
+#ifdef __cplusplus
+extern "C"
+#endif  // __cplusplus
+int EngineBind(ENGINE *e, const char *id);
+
+#endif  // KMSENGINE_BRIDGE_ENGINE_BIND_H_
---
+++ src/bridge/engine_setup.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KMSENGINE_BRIDGE_ENGINE_SETUP_H_
+#define KMSENGINE_BRIDGE_ENGINE_SETUP_H_
+
+#include <openssl/engine.h>
+
+namespace kmsengine {
+namespace bridge {
+
+// Initializes ENGINE substructures. Returns 1 on success and 0 if an error
+// occured.
+//
+// Used as the init_function in the OpenSSL engine. EngineBind is always called
+// prior to calling EngineInit.
+int EngineInit(ENGINE *e);
+
+// Cleans up ENGINE substructures. Returns 1 on success and 0 if an error
+// occured.
+//
+// Used as the finish_function in the OpenSSL engine..
+int EngineFinish(ENGINE *e);
+
+}  // namespace bridge
+}  // namespace kmsengine
+
+#endif  // KMSENGINE_BRIDGE_ENGINE_SETUP_H_
