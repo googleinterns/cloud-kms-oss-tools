e3a63cb5fdb2bacc9f2f733a6dde3dc4bf8d6b4a
Zachary Espiritu <zesp@google.com>
2020-06-29 09:39:24 -0400

    Add client interfaces for the KMS API

---
+++ src/backing/client/asymmetric_sign_request_test.cc
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "src/backing/client/asymmetric_sign_request.h"
+#include "src/backing/client/digest.h"
+
+namespace kmsengine {
+namespace backing {
+namespace client {
+namespace {
+
+TEST(AsymmetricSignRequestTest, ConstructorSetsExpectedFields) {
+  Digest digest(DigestType::kSha256, "some-digest");
+  AsymmetricSignRequest request("my-key-name", digest);
+  EXPECT_EQ(request.KeyName(), "my-key-name");
+  EXPECT_EQ(request.Digest().Type(), DigestType::kSha256);
+  EXPECT_EQ(request.Digest().Bytes(), "some-digest");
+}
+
+}  // namespace
+}  // namespace client
+}  // namespace backing
+}  // namespace kmsengine
---
+++ src/backing/client/asymmetric_sign_response.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KMSENGINE_BACKING_CLIENT_ASYMMETRIC_SIGN_RESPONSE_H_
+#define KMSENGINE_BACKING_CLIENT_ASYMMETRIC_SIGN_RESPONSE_H_
+
+#include <string>
+#include <utility>
+
+namespace kmsengine {
+namespace backing {
+namespace client {
+
+// Represents metadata for a AsymmetricSignResponse from the Key Management
+// Service API.
+class AsymmetricSignResponse {
+ public:
+  AsymmetricSignResponse(std::string signature)
+      : signature_(std::move(signature)) {}
+
+  std::string const& Signature() const { return signature_; }
+
+ private:
+  std::string signature_;
+};
+
+}  // namespace client
+}  // namespace backing
+}  // namespace kmsengine
+
+#endif  // KMSENGINE_BACKING_CLIENT_ASYMMETRIC_SIGN_RESPONSE_H_
