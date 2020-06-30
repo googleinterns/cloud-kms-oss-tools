2ec084267992205456ee49a44f3db26a2abc6481
Zachary Espiritu <zesp@google.com>
2020-06-30 16:16:20 -0400

    Add ClientContextFactory

---
+++ src/backing/client/grpc_client_context_factory.h
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KMSENGINE_BACKING_CLIENT_GRPC_CLIENT_CONTEXT_FACTORY_H_
+#define KMSENGINE_BACKING_CLIENT_GRPC_CLIENT_CONTEXT_FACTORY_H_
+
+#include <chrono>
+#include <memory>
+
+#include <grpcpp/grpcpp.h>
+
+#include "src/backing/client/clock.h"
+#include "src/backing/client/grpc_client_options.h"
+
+namespace kmsengine {
+namespace backing {
+namespace client {
+
+// Factory class for making a `grpc::ClientContext` from a set of
+// `GrpcClientOptions`.
+//
+// Defined separately from `GrpcClient` primarly for testing.
+class GrpcClientContextFactory {
+ public:
+  GrpcClientContextFactory(std::shared_ptr<GrpcClientOptions> options,
+                           std::shared_ptr<SystemClock> clock)
+      : client_options_(options), clock_(clock) {}
+
+  // Instantiates a `grpc::ClientContext` for use in making gRPC calls based on
+  // settings from the `GrpcClientOptions` with this `GrpcClient`.
+  //
+  // This method does not directly return a `grpc::ClientContext` instance
+  // since `grpc::ClientContext` is non-movable. (See related discussion at
+  // https://github.com/grpc/grpc/issues/16680.)
+  std::unique_ptr<grpc::ClientContext> MakeContext();
+
+ private:
+  std::shared_ptr<GrpcClientOptions> client_options_;
+  std::shared_ptr<SystemClock> clock_;
+};
+
+}  // namespace client
+}  // namespace backing
+}  // namespace kmsengine
+
+#endif  // KMSENGINE_BACKING_CLIENT_GRPC_CLIENT_CONTEXT_FACTORY_H_
---
+++ src/backing/client/grpc_client_context_factory_test.cc
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "src/backing/client/grpc_client_context_factory.h"
+#include "src/backing/client/testing_util/fake_clock.h"
+
+namespace kmsengine {
+namespace backing {
+namespace client {
+namespace {
+
+using ::kmsengine::backing::client::testing_util::FakeSystemClock;
+
+TEST(GrpcClientContextFactoryTest, MakeContextSetsDeadline) {
+  const std::chrono::milliseconds kTestTimeoutDurations[] = {
+    std::chrono::milliseconds(150),
+    std::chrono::milliseconds(500),
+    std::chrono::seconds(10),
+    std::chrono::minutes(1),
+  };
+
+  for (auto timeout_duration : kTestTimeoutDurations) {
+    // In the CI environment `grpc::GoogleDefaultCredentials()` may assert. Use
+    // the insecure credentials to initialize the options in any unit test.
+    std::shared_ptr<GrpcClientOptions> options =
+        std::make_shared<GrpcClientOptions>(grpc::InsecureChannelCredentials());
+    options->set_timeout_duration(timeout_duration);
+
+    std::shared_ptr<FakeSystemClock> fake_clock =
+        std::make_shared<FakeSystemClock>();
+    GrpcClientContextFactory factory(options, fake_clock);
+
+    // Some arbitrary amount of time passes after factory is instantiated.
+    SystemClock real_clock;
+    SystemClock::time_point time(real_clock.Now());
+    fake_clock->SetTime(time);
+
+    std::unique_ptr<grpc::ClientContext> context = factory->MakeContext();
+
+    auto expected = time + timeout_duration;
+    EXPECT_EQ(context->deadline(), expected);
+  }
+}
+
+}  // namespace
+}  // namespace client
+}  // namespace backing
+}  // namespace kmsengine
