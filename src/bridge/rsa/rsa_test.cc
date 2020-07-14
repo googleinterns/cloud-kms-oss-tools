bb1a52ff44e6b6e21435f767680bde52a8f8dc54
Zachary Espiritu <zesp@google.com>
2020-07-14 16:56:34 -0400

    Add more RSA tests

--- src/bridge/rsa/rsa_test.cc
+++ src/bridge/rsa/rsa_test.cc
@@ -27,6 +27,7 @@
 #include "src/bridge/rsa/rsa.h"
 #include "src/testing_util/mock_rsa_key.h"
 #include "src/testing_util/test_matchers.h"
+#include "src/testing_util/openssl_assertions.h"
 
 namespace kmsengine {
 namespace bridge {
@@ -35,72 +36,8 @@ namespace {
 
 using ::testing::Return;
 using ::testing::StrEq;
-using ::testing::HasSubstr;
-using ::testing::Matches;
 using ::kmsengine::testing_util::IsOk;
 using ::kmsengine::testing_util::MockRsaKey;
-
-testing::AssertionResult IsOpenSslSuccess(const char* expr,
-                                          bool openssl_return) {
-  if (openssl_return) return testing::AssertionSuccess();
-
-  const char *file, *data;
-  int line, flags;
-  auto err = ERR_get_error_line_data(&file, &line, &data, &flags);
-  if (err) {
-    return testing::AssertionFailure()
-           << expr << " unexpectedly returned false and produced OpenSSL "
-           << "error...\n"
-           << "         Expected: success\n"
-           << "           Actual: error in " << file << ":" << line << "\n"
-           << "                   [" << data << "]";
-  } else {
-    return testing::AssertionFailure()
-           << expr << " unexpectedly returned false"
-           << "         Expected: success\n"
-           << "           Actual: failure (no OpenSSL error)";
-  }
-}
-
-testing::AssertionResult IsOpenSslFailure(const char* return_expr,
-                                          const char* status_expr,
-                                          bool openssl_return,
-                                          const char* substr_error) {
-  if (openssl_return) {
-    return testing::AssertionFailure()
-           << return_expr << " should have returned false, but returned "
-           << "true...\n"
-           << "         Expected: failure with OpenSSL error\n"
-           << "           Actual: success";
-  }
-
-  const char *file, *data;
-  int line, flags;
-  auto err = ERR_get_error_line_data(&file, &line, &data, &flags);
-  if (!err) {
-    return testing::AssertionFailure()
-           << return_expr << " returned false, but no OpenSSL "
-           << "         Expected: failure with OpenSSL error\n"
-           << "           Actual: failure, but no OpenSSL error";
-  }
-
-  if (Matches(HasSubstr(substr_error))(data)) {
-    return testing::AssertionSuccess();
-  } else {
-      return testing::AssertionFailure()
-         << return_expr << " returned false and produced OpenSSL error, but "
-         << "OpenSSL error did not contain expected substring...\n"
-         << "         Expected: error containing '" << substr_error << "'\n"
-         << "           Actual: error in " << file << ":" << line << "\n"
-         << "                   [" << data << "]";
-  }
-}
-
-#define ASSERT_OPENSSL_SUCCESS(val) \
-  ASSERT_PRED_FORMAT1(IsOpenSslSuccess, val)
-
-#define ASSERT_OPENSSL_FAILURE(val, status) \
-  ASSERT_PRED_FORMAT2(IsOpenSslFailure, val, status)
 
 // Test fixture for calling initialization functions (normally called by the
 // `EngineBind` function) needed for the RSA callbacks to work.
@@ -114,7 +51,7 @@ class RsaTest : public ::testing::Test {
   }
 };
 
-TEST_F(RsaTest, SignWorks) {
+TEST_F(RsaTest, SignReturnsSignature) {
   std::string expected = "my signature";
 
   MockRsaKey rsa_key;
@@ -132,6 +69,40 @@ TEST_F(RsaTest, SignWorks) {
 
   std::string actual(reinterpret_cast<char *>(signature), signature_length);
   EXPECT_THAT(actual, StrEq(expected));
+}
+
+TEST_F(RsaTest, SignPropogatesErrors) {
+  std::string expected_error = "mock RsaKey::Sign failed";
+
+  MockRsaKey rsa_key;
+  EXPECT_CALL(rsa_key, Sign).WillOnce(Return(StatusOr<std::string>(
+      Status(StatusCode::kInternal, expected_error))));
+
+  auto rsa = MakeRsa();
+  ASSERT_THAT(AttachRsaKeyToOpenSslRsa(&rsa_key, rsa.get()), IsOk());
+
+  unsigned char digest[] = "sample digest";
+  unsigned int digest_length = std::strlen(reinterpret_cast<char *>(digest));
+  unsigned char *signature;
+  unsigned int signature_length;
+  EXPECT_OPENSSL_FAILURE(Sign(NID_sha256, digest, digest_length, signature,
+                              &signature_length, rsa.get()),
+                         expected_error);
+}
+
+TEST_F(RsaTest, SignHandlesMissingRsaKeys) {
+  std::string expected_error = "mock RsaKey::Sign failed";
+
+  auto rsa = MakeRsa();
+  ASSERT_THAT(AttachRsaKeyToOpenSslRsa(nullptr, rsa.get()), IsOk());
+
+  unsigned char digest[] = "sample digest";
+  unsigned int digest_length = std::strlen(reinterpret_cast<char *>(digest));
+  unsigned char *signature;
+  unsigned int signature_length;
+  EXPECT_OPENSSL_FAILURE(Sign(NID_sha256, digest, digest_length, signature,
+                              &signature_length, rsa.get()),
+                         "No Cloud KMS key associated with RSA struct");
 }
 
 }  // namespace
