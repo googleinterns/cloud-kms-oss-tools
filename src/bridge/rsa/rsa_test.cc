d30ee493edfd20e349bc9bcbc30d631f31b86fb3
Zachary Espiritu <zesp@google.com>
2020-07-15 14:24:33 +0000

    Clean up RSA_METHOD tests

--- src/bridge/rsa/rsa_test.cc
+++ src/bridge/rsa/rsa_test.cc
@@ -46,8 +46,6 @@ class RsaMethodTest : public ::testing::Test {
  protected:
   void SetUp() override {
     ASSERT_THAT(InitExternalIndicies(), IsOk());
-
-    rsa_method = MakeKmsRsaMethod();
     ASSERT_THAT(rsa_method, NotNull());
   }
 
@@ -55,10 +53,15 @@ class RsaMethodTest : public ::testing::Test {
     FreeExternalIndicies();
   }
 
-  // Explicit `nullptr` needed here since these `unique_ptr`'s include a custom
-  // `Deleter` type, and `unique_ptr`'s with custom Deleter's have no default
-  // constructor.
-  OpenSslRsaMethod rsa_method {nullptr, nullptr};
+  // Convenience function for making an RSA struct with the Cloud KMS RSA_METHOD
+  // implementation already attached.
+  OpenSslRsa MakeRsaWithKmsMethod() {
+    auto rsa = MakeRsa();
+    if (rsa) RSA_set_method(rsa.get(), rsa_method.get());
+    return rsa;
+  }
+
+  OpenSslRsaMethod rsa_method {MakeKmsRsaMethod()};
 };
 
 TEST_F(RsaMethodTest, FinishCleansUpRsaKey) {
@@ -78,9 +81,12 @@ TEST_F(RsaMethodTest, FinishCleansUpRsaKey) {
 }
 
 TEST_F(RsaMethodTest, SignReturnsSignature) {
-  auto rsa = MakeRsa();
+  // Construct the RSA struct within the test body as opposed to the fixture
+  // so it cleans itself up before the end of the test. This is important so
+  // that it deletes the `MockRsaKey` before the test body ends (otherwise a
+  // testing error will be raised).
+  auto rsa = MakeRsaWithKmsMethod();
   ASSERT_THAT(rsa, NotNull());
-  RSA_set_method(rsa.get(), rsa_method.get());
 
   std::string expected = "my signature";
   auto rsa_key = new MockRsaKey();
@@ -91,18 +97,16 @@ TEST_F(RsaMethodTest, SignReturnsSignature) {
   unsigned int msg_length = std::strlen(reinterpret_cast<char *>(msg));
   unsigned char signature[expected.length()];
   unsigned int signature_length;
-  ASSERT_OPENSSL_SUCCESS(
-      RSA_sign(NID_sha256, msg, msg_length, signature, &signature_length,
-               rsa.get()));
+  ASSERT_OPENSSL_SUCCESS(RSA_sign(NID_sha256, msg, msg_length, signature,
+                                  &signature_length, rsa.get()));
 
   std::string actual(reinterpret_cast<char *>(signature), signature_length);
   EXPECT_THAT(actual, StrEq(expected));
 }
 
 TEST_F(RsaMethodTest, SignHandlesRsaKeySignMethodErrors) {
-  auto rsa = MakeRsa();
+  auto rsa = MakeRsaWithKmsMethod();
   ASSERT_THAT(rsa, NotNull());
-  RSA_set_method(rsa.get(), rsa_method.get());
 
   auto expected_error_message = "mock RsaKey::Sign failed";
   auto rsa_key = new MockRsaKey();
@@ -118,10 +122,8 @@ TEST_F(RsaMethodTest, SignHandlesRsaKeySignMethodErrors) {
 }
 
 TEST_F(RsaMethodTest, SignHandlesMissingRsaKeys) {
-  auto rsa = MakeRsa();
+  auto rsa = MakeRsaWithKmsMethod();
   ASSERT_THAT(rsa, NotNull());
-  RSA_set_method(rsa.get(), rsa_method.get());
-
   ASSERT_THAT(AttachRsaKeyToOpenSslRsa(nullptr, rsa.get()), IsOk());
 
   unsigned char msg[] = "sample digest";
@@ -132,8 +134,8 @@ TEST_F(RsaMethodTest, SignHandlesMissingRsaKeys) {
 }
 
 TEST_F(RsaMethodTest, SignHandlesBadNidDigestTypes) {
-  auto rsa = MakeRsa();
-  RSA_set_method(rsa.get(), rsa_method.get());
+  auto rsa = MakeRsaWithKmsMethod();
+  ASSERT_THAT(rsa, NotNull());
 
   auto rsa_key = new MockRsaKey();
   EXPECT_CALL(*rsa_key, Sign).Times(0);
@@ -142,11 +144,10 @@ TEST_F(RsaMethodTest, SignHandlesBadNidDigestTypes) {
   // Use MD5 as our "bad digest type" example, since it's not supported by
   // Cloud KMS (and since it's an insecure algorithm, it probably won't be
   // supported in the future).
-  constexpr int kBadDigestNid = NID_md5;
   unsigned char msg[] = "sample digest";
   unsigned int msg_length = std::strlen(reinterpret_cast<char *>(msg));
   EXPECT_OPENSSL_FAILURE(
-      RSA_sign(kBadDigestNid, msg, msg_length, nullptr, nullptr, rsa.get()),
+      RSA_sign(NID_md5, msg, msg_length, nullptr, nullptr, rsa.get()),
       "Unsupported digest type");
 }
 
